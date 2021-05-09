2398c50ef4fa955b36d7646d9c1ac267a7a274f0
JOHAESEONG <81471744+lo-ji@users.noreply.github.com>
2021-05-09 17:47:56 +0900

    .

---
+++ 20151023_r57_matgo/Assets/scripts/DestroySelf.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class DestroySelf : MonoBehaviour
+{
+
+    public float DestroyTime = 2.0f;
+    void Start()
+    {
+        Destroy(gameObject, DestroyTime);
+    }
+}
